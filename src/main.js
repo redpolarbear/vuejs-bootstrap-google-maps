72ba44a475fd4357ac4d0ff78928ea75a730c916
redpolarbear <mrredpolarbear@gmail.com>
2017-10-23 16:49:25 -0700

    initiate

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+import App from './App'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// temp solution before bootstrap-vue beta10 released
+let originalVueComponent = Vue.component
+Vue.component = function (name, definition) {
+  if (name === 'bFormCheckboxGroup' || name === 'bCheckboxGroup' ||
+      name === 'bCheckGroup' || name === 'bFormRadioGroup') {
+    definition.components = {bFormCheckbox: definition.components[0]}
+  }
+  originalVueComponent.apply(this, [name, definition])
+} //
+
+Vue.use(BootstrapVue)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyCc1149CMxTx2holKBrQmsiKgSafW0MPz0',
+    libraries: 'places' // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
